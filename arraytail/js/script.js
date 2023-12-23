7c72fbb7200a68685f613cdc19fa5e3b2610a477
giamaf <gianluca.maffucci@hotmail.com>
2023-12-23 15:32:13 +0100

    js_analysis

--- arraytail/js/script.js
+++ arraytail/js/script.js
@@ -1,1 +1,17 @@
 console.log('JS OK')
+
+//--- La coda dell'array --- \\
+
+// Creiamo un array chiedendo all'utente quanti elementi dovrà contenere e generando tanti numeri casuali (compresi nell'intervallo da 1 a 100) per quanti sono gli elementi da inserire.
+// Stampiamo poi gli ultimi 5 elementi dell'array
+
+// Bonus:
+// Chiediamo all'utente quanti elementi dell'array dovremo stampare
+
+//------------------------------------------------------------------------ \\ 
+
+//todo ANALISI
+
+//todo 1 - Creo array vuoto
+//todo 2 - Chiedo all'utente quanti elementi inserire
+//todo 3 - Randomizzo il numero di elementi scelto dall'utente e li inserisco nell'array
