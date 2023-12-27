bfc6ca3eb320d00563389f2ae28ca6dd2f0990fb
giamaf <gianluca.maffucci@hotmail.com>
2023-12-27 18:16:24 +0100

    js_added logic rules

--- arraytail/js/script.js
+++ arraytail/js/script.js
@@ -1,4 +1,4 @@
-console.log('JS OK')
+// console.log('JS OK')
 
 //--- La coda dell'array --- \\
 
@@ -15,3 +15,30 @@ console.log('JS OK')
 //todo 1 - Creo array vuoto
 //todo 2 - Chiedo all'utente quanti elementi inserire
 //todo 3 - Randomizzo il numero di elementi scelto dall'utente e li inserisco nell'array
+
+
+//todo 1 - Creo array vuoto
+const arrayNum = [];
+console.table(arrayNum);
+
+//todo 2 - Chiedo all'utente quanti elementi inserire e leggo il dato scelto
+const userChoice = document.getElementById('select-choice');
+const confirmButton = document.getElementById('confirm-button');
+// console.log(userChoice, confirmButton);
+
+confirmButton.addEventListener('click', function () {
+    userChoiceValue = userChoice.value;
+    console.log('Numero scelto: ', userChoiceValue);
+
+    //todo 3 - Randomizzo il numero di elementi scelto dall'utente e li inserisco nell'array
+    for (let i = 0; i < userChoiceValue; i++) {
+        const randomization = arrayNum.push(Math.floor(Math.random() * 100) + 1);
+
+        if ([i] > 4) {
+            console.log(arrayNum[i]);
+        }
+
+    }
+
+    console.table(arrayNum);
+})
